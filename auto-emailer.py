8fad814464894dbeacfd3151338e6b0e54f42ea0
MrGri00 <marcgarciat2000@gmail.com>
2025-01-10 15:33:38 +0100

    Add attachments

--- auto-emailer.py
+++ auto-emailer.py
@@ -1,3 +1,5 @@
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from enum import Enum
@@ -41,6 +43,16 @@ def send_email(subject, body, recipient, attachments=[]):
     msg['To'] = recipient
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
+    for attachment in attachments:
+        try:
+            with open(attachment, 'rb') as file:
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(file.read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', f'attachment; filename={os.path.basename(attachment)}')
+                msg.attach(part)
+        except Exception as e:
+            print(f'Failed to attach file {attachment}: {e}')
     try:
         with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls()
@@ -53,10 +65,18 @@ def send_email(subject, body, recipient, attachments=[]):
 def format_email(body, name):
     return body.replace('[NAME]', name)
 
+def get_attachments(directory):
+    attachments = []
+    for filename in os.listdir(directory):
+        if not filename.startswith('.'):
+            attachments.append(os.path.join(directory, filename))
+    return attachments
+
 # Main
 contacts = read_csv(file_path)
 email_CA = read_txt('docs/email_CA.txt')
 email_ES = read_txt('docs/email_ES.txt')
+all_attachments = get_attachments('attachments')
 
 for contact in contacts:
     try:
@@ -66,6 +86,6 @@ for contact in contacts:
     send_email(
         subject='Test email',
         body=format_email(locale_body, contact[name_col]),
-        #recipient=contact[recipient_col]
-        recipient=EMAIL_ADDRESS
+        recipient=contact[recipient_col],
+        attachments=all_attachments
     )
